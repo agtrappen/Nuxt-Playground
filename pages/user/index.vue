419db644e62381a9415ae677d3b931e934f753ca
Alexander <alexinantore@gmail.com>
2019-10-07 11:12:25 +0200

    added src files

---
+++ pages/user/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <div>
+            <logo />
+            <h1 class="title text-white">
+                page 1
+            </h1>
+            <h2 class="subtitle text-white">
+                dit is page 1
+            </h2>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Logo from '~/components/Logo.vue'
+    
+    export default {
+        components: {
+            Logo
+        }
+    }
+</script>
+  
